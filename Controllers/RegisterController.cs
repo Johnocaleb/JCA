b2a247dcd80d563d52df9e96b53092f25682380f
John caleb <Calebjohn3112@gmail.com>
2022-08-23 16:12:04 +0100

    Add files via upload

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CHAT.Controllers
+{
+    public class RegisterController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
